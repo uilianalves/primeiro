9f9c06cfeb0d99a704a65928b7d9919808de3e7c
Uilian Alves <uilianalves@gmail.com>
2020-08-30 15:55:12 -0300

    b

--- src/main/java/com/uilian/primeiro/PrimeiroApplication.java
+++ src/main/java/com/uilian/primeiro/PrimeiroApplication.java
@@ -3,6 +3,9 @@ package com.uilian.primeiro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+*b
+*/
 @SpringBootApplication
 public class PrimeiroApplication {
 
